653802bb7a194dce85df2ae61565376a20b6a8a5
jonathan-pasco-arnone <60712726+jonathan-pasco-arnone@users.noreply.github.com>
2023-10-16 21:47:11 +0000

    need tf to work first

--- testing-resources/search.py
+++ testing-resources/search.py
@@ -7,13 +7,22 @@ import os
 import searchdata
 
 CRAWL_PATH = "/workspaces/final_project_COMP1405/testing-resources/crawler_data"
+SEARCH_PATH = "/workspaces/final_project_COMP1405/testing-resources/crawler_data/search_words"
 
 def search(phrase, boost):
     """ Determines the top ten searchs for the query inputted """
+
+    # Creates temporary folder for the words to be held in
+    os.mkdir(SEARCH_PATH)
+    seacrh_title = open(SEARCH_PATH + "/title_and_link.txt", "w+", encoding="utf8")
+    # This is so the search data program can access the data for tf-idf calculations
+    seacrh_title.write(" \nsearch")
+    seacrh_title.close()
+    
     # Makes the file with all the inputed words
-    word_file = open(CRAWL_PATH + "/../search_words.txt", "w+", encoding="utf8")
+    word_file = open(SEARCH_PATH + "/file_text.txt", "w+", encoding="utf8")
     word_file.writelines(phrase)
-    word_file.seek(0)
+    word_file.close()    
 
     words = phrase.split()
 
@@ -32,8 +41,14 @@ def search(phrase, boost):
         denominator = 0
         for word in words:
             d_vector = searchdata.get_tf_idf(word, file_link)
-            q_vector = searchdata.get_tf_idf(word, CRAWL_PATH + "/../search_words.txt")
+            q_vector = searchdata.get_tf_idf(word, "search")
             numerator += q_vector * d_vector
+    
+    # Removes the temporary directory and all the files included
+    if os.path.exists(SEARCH_PATH):
+        for file in os.listdir(SEARCH_PATH):
+            os.remove(SEARCH_PATH + "/" + str(file))
+        os.rmdir(SEARCH_PATH)
     
 
 
